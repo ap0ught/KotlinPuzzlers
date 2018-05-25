23ebd2dd32bf58e1ffcd43ba96434133fbc15916
roman.donchenko <roman.donchenko@thinkmobiles.com>
2018-05-26 00:00:18 +0300

    small changes

--- src/part1/6_long_line.kts
+++ src/part1/6_long_line.kts
@@ -1,18 +1,17 @@
 package part1
 
-val reallyVeryLongAndComplicatedCalculatedValue = 2
-val anotherReallyVeryLongAndComplicatedCalculatedValue = 3
+val youNameVariablesSuchLongDontYou = 2
+val inMostCasesAnyLongNameCanBeShortened = 3
 
 val result =
-    1
-      + reallyVeryLongAndComplicatedCalculatedValue
-         +  anotherReallyVeryLongAndComplicatedCalculatedValue
+    1 and youNameVariablesSuchLongDontYou
+            +  inMostCasesAnyLongNameCanBeShortened
 
 
 println(result)
 
 // What will it prints?
-// 1. 1
-// 2. 2
+// 1. 0
+// 2. 1
 // 3. 3
 // 4. 6
